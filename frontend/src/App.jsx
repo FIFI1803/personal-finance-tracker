1a0cc47812088d8860a1a8d009de42d5db5a3c6f
Filip <filipgalach@icloud.com>
2025-12-13 13:34:54 +0000

    Pulling transaction data from backend and display in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { getTransactions } from "./api/transactions";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [transactions, setTransactions] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    getTransactions().then((response) => {
+      setTransactions(response.data);
+    });
+  }, []);
+
+ return (
+    <div>
+      <h1>Finance Tracker</h1>
+
+      <ul>
+        {transactions.map((tx) => (
+          <li key={tx._id}>
+            {tx.description}: ${tx.amount} on {new Date(tx.date).toLocaleDateString()}
+          </li>
+        ))}
+      </ul> 
+    </div>
+  );
 }
 
-export default App
+export default App;
